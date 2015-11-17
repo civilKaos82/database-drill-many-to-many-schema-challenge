df7a40c953de50e9b15cfc308fcce5d6733deec5
Torey Hickman <torey@toreyhickman.com>
2015-11-17 12:30:49 -0600

    Create seed data

---
+++ runner.rb
@@ -0,0 +1,7 @@
+require_relative 'user'
+require_relative 'product'
+require_relative 'review'
+require_relative 'favoriting'
+
+# create the users, products, reviews, and favoritings
+require_relative 'seeds'
